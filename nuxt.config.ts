7301d276055c1d4885e446b919dc3b7596051a3b
savvy <savvyversa@gmail.com>
2023-09-26 23:26:26 +0330

    fix: add blog core

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 import {defineNuxtConfig} from "nuxt/config";
+import {config} from 'dotenv'
+
+config();
 export default defineNuxtConfig({
     extends: [
-        'digimarket-nuxt-layer-core'
+        'digimarket-nuxt-layer-blog'
     ]
 })
